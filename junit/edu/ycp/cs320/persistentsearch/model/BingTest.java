35d40cab21596a94a6fb2ffbdaf86bf550051042
jcoy1 <jcoy1@ycp.edu>
2012-04-22 15:07:42 -0400

    BingTest

--- junit/edu/ycp/cs320/persistentsearch/model/BingTest.java
+++ junit/edu/ycp/cs320/persistentsearch/model/BingTest.java
@@ -1,12 +1,39 @@
 package edu.ycp.cs320.persistentsearch.model;
 
-public class BingTest implements Website {
+import static org.junit.Assert.*;
 
-	@Override
-	public ResultCollection performSearch(SearchTerm sT,
-			ResultCollection resultsToUpdate) throws SearchException {
-		// TODO Auto-generated method stub
-		return null;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BingTest{
+
+	private Bing bing;
+	private Bing bing1;
+	private ResultCollection results;
+	private SearchTerm st;
+	
+	@Before
+	public void setUp()
+	{
+		bing = new Bing();
+		bing1 = new Bing();
+		results = new ResultCollection();
+		st = new SearchTerm("Giants");
+	}
+	
+	@Test
+	public void testPerformSearch() throws Exception
+	{
+		assertEquals(0, results.getResults().size());
+		results = bing.performSearch(st, results);
+		assertFalse(results.getResults().size() == 0);
+	}
+	
+	@Test
+	public void testEquals() throws Exception
+	{
+		assertTrue(bing.equals(bing1));
+		assertTrue(bing.equals(bing));
 	}
 
 }
